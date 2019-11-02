619755c38000847571512d40e531b600ae7ea808
Loki <Loki.Astari@gmail.com>
2019-11-02 13:12:44 -0700

    Create a header only version

---
+++ ThorsStream/ThorsStreamConfig.h
@@ -0,0 +1,87 @@
+/* src/SocketStream/ThorsStreamConfig.h.  Generated from ThorsStreamConfig.h.in by configure.  */
+/* src/SocketStream/ThorsStreamConfig.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the Boost library is available */
+/* #undef HAVE_BOOST */
+
+/* define if the Boost::Python library is available */
+/* #undef HAVE_BOOST_PYTHON */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `curl' library (-lcurl). */
+#define HAVE_LIBCURL 1
+
+/* Define to 1 if you have the `tcl' library (-ltcl). */
+/* #undef HAVE_LIBTCL */
+
+/* Define to 1 if you have the `tk' library (-ltk). */
+/* #undef HAVE_LIBTK */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* If available, contains the Python version number currently in use. */
+#define HAVE_PYTHON "2.7"
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "thorsstream"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "Loki.Astari+ThorsAnvil@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "ThorsStream"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ThorsStream 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "thorsstream"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.1"
+
+/* Header Only Version Enabled */
+#define HEADER_ONLY 1
+
+/* For header only convery to inline */
+#define HEADER_ONLY_INCLUDE inline
+
